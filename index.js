2537452cdaece559c932149528463fc665de7270
Neil García Vargas <Neil@Neils-MacBook-Pro.local>
2016-06-26 22:47:24 -0600

    new port

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(80, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
